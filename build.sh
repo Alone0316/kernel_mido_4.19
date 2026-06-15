2332ab946b4785081cd98a1713914178dff580a4
Alone0316 <70204635+Alone0316@users.noreply.github.com>
2026-06-15 13:54:33 +0530

    Build.sh: Update clang

--- build.sh
+++ build.sh
@@ -115,14 +115,15 @@ if [ "$TOOLCHAIN" == gcc ]; then
 	export STRIP="$HOME/gcc64/aarch64-elf/bin/strip"
 	export KBUILD_COMPILER_STRING=$("$HOME"/gcc64/bin/aarch64-elf-gcc --version | head -n 1)
 elif [ "$TOOLCHAIN" == clang ]; then
-	if [ ! -d "$HOME/proton_clang" ]
+	if [ ! -d "$HOME/clang" ]
 	then
-		echo -e "$green << cloning proton clang >> \n $white"
-		git clone --depth=1 https://github.com/kdrag0n/proton-clang.git "$HOME"/proton_clang
+		echo -e "$green << fetching clang >> \n $white"
+		wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/tags/android-15.0.0_r36/clang-r522817.tar.gz -O "aosp-clang.tar.gz"
+		mkdir clang && tar -xf aosp-clang.tar.gz -C clang && rm -rf aosp-clang.tar.gz
 	fi
-	export PATH="$HOME/proton_clang/bin:$PATH"
-	export STRIP="$HOME/proton_clang/aarch64-linux-gnu/bin/strip"
-	export KBUILD_COMPILER_STRING=$("$HOME"/proton_clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+	export PATH="${PWD}/clang/bin:$PATH"
+	export STRIP="$HOME/clang/aarch64-linux-gnu/bin/strip"
+	export KBUILD_COMPILER_STRING=$("${PWD}"/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
 fi
 
 # Setup build process
